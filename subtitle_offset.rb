58edae994b6ac41767e56f30c6ab18e4944eb483
Thomas Lecavelier <thomas@lecavelier.name>
2009-07-27 15:09:01 +0200

    First version of subtitle_offset, script written in 2h during my lunch_time, so please: be gentle about the code.
    First piece of code written after the death of my wife's bunny:
    Capucine. You'll miss us, U know…

---
+++ subtitle_offset.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/ruby -w
+## subtitle_offset : shift time
+##   usage: subtitle_offset.rb your_srt.srt seconds_to_shift
+## 
+## By Thomas "ook!" Lecavelier - http://thomas.lecavelier.name
+##
+## In memoriam Capucine, the most fabulous rabbit that I met (1996-09-01 - 2009-07-26)
+# Under the WTFPL - http://sam.zoy.org/wtfpl/
+
+TIMESTAMP_RX = /(\d{2}:\d{2}:\d{2})/
+
+def usage()
+  # yeah, that's a complet robbery from sunny's code: http://github.com/sunny/mariokartwiit/
+  puts open(__FILE__).read.grep(/^## ?/).join.gsub(/^## ?/, '')  
+end
+
+def check_args()
+  $*.size >= 2 && File.exist?($*[0])
+end
+
+def pad(int, pad = 2)
+  int < 10 ? "0#{int.to_s}" : int.to_s
+end
+
+def shift(time_index, shift_seconds = 40)
+  return '!!:!!:!!' unless time_index
+
+  elems = time_index.split(':')
+  elems.each_with_index { |d, i| elems[i] = d.to_i }
+  elems[-1] += shift_seconds
+  if 59 < elems[-1]
+    elems[-1] -= 60
+    elems[-2] +=  1
+  end
+  if  0 > elems[-1]
+    elems[-1] += 60
+    elems[-2] -=  1
+  end
+  if 59 < elems[-2]
+    elems[-2] -= 60
+    elems[-3] +=  1
+  end
+  if  0 > elems[-2]
+    elems[-2] += 60
+    elems[-3] -=  1
+  end
+  if 23 < elems[-3]
+    elems[-3] -= 24
+  end
+  if  0 > elems[-3]
+    #elems[-3] += 24
+    return time_index # don't try to mess with the index!
+  end
+  
+  "#{pad(elems[0])}:#{pad(elems[1])}:#{pad(elems[2])}"
+end
+
+unless check_args
+  usage
+  exit 42
+end
+File.open($*[0], 'r') do |orig|
+  out = File.open("#{$*[0]}.shifted", 'w')
+  orig.each do |line|
+    if line =~ TIMESTAMP_RX
+
+      orig_from, orig_to = $1, $2
+      out << line.gsub(TIMESTAMP_RX) { |m| "#{shift(m, $*[1].to_i)}" } 
+    else
+      out << line
+    end
+  end
+  out.close
+end
+
+
